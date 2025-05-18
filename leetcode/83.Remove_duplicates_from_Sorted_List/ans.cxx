79b496a06a0944907d0452440288ef9c89566863
sangjo <sangjo.kim@sonatus.com>
2025-05-18 21:40:48 +0900

    83

--- leetcode/83.Remove_duplicates_from_Sorted_List/ans.cxx
+++ leetcode/83.Remove_duplicates_from_Sorted_List/ans.cxx
@@ -11,22 +11,24 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(!head) {
+        if(head == nullptr && head->next == nullptr) {
             return head;
         }
         ListNode* here = head;
 
         while(here->next != nullptr) {
-            std::cout <<head->val << " " << head->next->val << std::endl;
+            // std::cout <<head->val << " " << head->next->val << std::endl;
             if(here->val == here->next->val) {
                 ListNode* temp = here->next;
                 here->next = here->next->next;
                 delete temp;
+            } else {
+
+                here = here->next;
             }
-            here = here->next;
             
         }
 
-        return here;
+        return head;
     }
 };
